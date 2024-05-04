91ae0115ab0c0a0df1d4dddaf65381f98a7dda84
mohits-git <mohitkhatri2805@gmail.com>
2024-05-04 15:17:45 +0530

    feat: added html page and setup websockets to Connect client and Create and Join a Game

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const app = require('express')();
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
+app.listen(9091, () => console.log("http: listening at 9091"));
+
 const http = require('http');
 const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
-httpServer.listen(9090, () => console.log("http: Listening at 9090"));
+httpServer.listen(9090, () => console.log("ws: Listening at 9090"));
 
 const clients = {};
+const games = {};
 
 const wsServer = new websocketServer({
     "httpServer": httpServer,
@@ -15,7 +20,49 @@ wsServer.on("request", request => {
     connection.on("close", () => console.log("Closed!!"));
     connection.on("message", message => {
         const result = JSON.parse(message.utf8Data);
-        console.log(result);
+
+        if (result.method === "create") {
+            const cId = result.clientId;   
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+                "clients": [],
+            }
+            
+            const payload = {
+                "method": "create",
+                "game": games[gameId]
+            }
+                
+            const con = clients[cId].connection;
+            con.send(JSON.stringify(payload));
+        }
+
+        if(result.method === "join") {
+            const gId = result.gameId;
+            const cId = result.clientId;
+
+            const game = games[gId];
+            if(game.clients.length >= 3) {
+                return;
+            }
+            const color = {"0": "red", "1": "green", "2": "blue" }[game.clients.length];
+            game.clients.push({
+                "clientId": cId,
+                "color": color
+            });
+
+            const payload = {
+                "method": "join",
+                "game": game
+            };
+
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payload));
+            });
+        }
+
     });
 
     const clientId = guid();
